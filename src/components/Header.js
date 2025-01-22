1cdece3b517c88820ee737b7bd131cfbe1cd0dfa
br7t <buratinivictor@gmail.com>
2025-01-22 00:19:47 -0300

    change name

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header(){
         <header className="bg-white h-[4.5rem] text-blue-900 flex items-center p-5 font-semibold text-[24px] shadow-sm">
             <div className="flex gap-5 items-center ">
                 <AiOutlineLink className="text-[30px]"/>
-                Shortfy
+                Encurta Já
             </div>
         </header>
     )
