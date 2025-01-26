ff6a20813d3249fdcbdd626189a3be66f9f91d5f
br7t <buratinivictor@gmail.com>
2025-01-26 00:03:04 -0300

    trying to fix the ads 1.1

--- src/app/_document.js
+++ src/app/_document.js
@@ -6,11 +6,8 @@ class MyDocument extends Document {
       <Html lang="pt-br">
         <Head>
           {/* Adicionar o script do Google AdSense */}
-          <script
-            async
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1320242016578102"
-            crossorigin="anonymous"
-          ></script>
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1320242016578102"
+     crossorigin="anonymous"></script>
         </Head>
         <body>
           <Main />
